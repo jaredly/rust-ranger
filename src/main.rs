92e9dbe1e4b6f4ed4e8d4bc70cc8fdad245e79be
Jared Forsyth <jared@jaredforsyth.com>
2019-08-12 08:59:47 -0600

    better physics

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,9 @@ use nphysics2d::world::World as PhysicsWorld;
 
 use std::sync::Arc;
 
-const BOX_SIZE_WIDTH: f32 = 250.0;
-const BOX_SIZE_HEIGHT: f32 = 20.0;
-const BALL_RADIUS: f32 = 10.0;
+const BOX_SIZE_WIDTH: f32 = 3.0;
+const BOX_SIZE_HEIGHT: f32 = 0.2;
+const BALL_RADIUS: f32 = 0.1;
 
 #[derive(Component)]
 struct Ground(ColliderHandle);
@@ -45,6 +45,8 @@ impl<'a> System<'a> for Draw {
     fn run(&mut self, (rl, physics, grounds, bodies): Self::SystemData) {
         use specs::Join;
 
+        let scale = 100.0;
+
         // Finally the ecs part. Iterate thorugh ground and body and draw them.
         for (ground,) in (&grounds,).join() {
             if let Some(collider) = physics.collider(ground.0) {
@@ -52,11 +54,9 @@ impl<'a> System<'a> for Draw {
                 // have to multiply with the origin to get the world position.
                 let p = collider.position() * Point2::new(0.0, 0.0);
                 // In reality, the shape would be part of the resources of the system. We cheat here
-                rl.draw_rectangle(
-                    p.x as i32,
-                    (p.y - BOX_SIZE_HEIGHT) as i32,
-                    BOX_SIZE_WIDTH as i32,
-                    BOX_SIZE_HEIGHT as i32,
+                rl.draw_rectangle_v(
+                    ((p.x - BOX_SIZE_WIDTH / 2.0)  * scale, (p.y - BOX_SIZE_HEIGHT / 2.0) * scale),
+                    (BOX_SIZE_WIDTH  * scale, BOX_SIZE_HEIGHT * scale),
                     raylib::Color::BLACK,
                 );
             }
@@ -65,9 +65,11 @@ impl<'a> System<'a> for Draw {
         for (ball,) in (&bodies,).join() {
             if let Some(body) = physics.rigid_body(ball.0) {
                 let p = body.position() * Point2::new(0.0, 0.0);
-                rl.draw_circle(p.x as i32, p.y as i32, BALL_RADIUS, raylib::Color::RED);
+                rl.draw_circle_v((p.x *scale, p.y * scale), BALL_RADIUS * scale, raylib::Color::RED);
             }
         }
+
+        rl.draw_fps(5, 5);
     }
 }
 
@@ -80,11 +82,11 @@ fn main() {
     physics_world.set_gravity(Vector2::new(0.0, 9.81));
 
     // Create the ground
-    let ground_shape = ShapeHandle::new(Cuboid::new(Vector2::new(BOX_SIZE_WIDTH, BOX_SIZE_HEIGHT)));
+    let ground_shape = ShapeHandle::new(Cuboid::new(Vector2::new(BOX_SIZE_WIDTH / 2.0, BOX_SIZE_HEIGHT / 2.0)));
 
     // Add ground to system
     let ground_handle = ColliderDesc::new(ground_shape)
-        .translation(Vector2::new(320.0, 240.0))
+        .translation(Vector2::new(3.20, 3.40))
         .build(&mut physics_world)
         .handle();
 
@@ -95,12 +97,13 @@ fn main() {
     let mut rigid_body = RigidBodyDesc::new().collider(&collider);
 
     // Note that for rust the last statement without a semicolon is the return
-    let ball_handles: Vec<_> = (0..5)
+    let ball_handles: Vec<_> = (0..15)
         .map(|i| {
-            let x = 320.0 + i as f32 * ((BALL_RADIUS + collider.get_margin()) * 2.0 + 0.1);
+            let x = 3.0 + i as f32 * ((BALL_RADIUS + collider.get_margin()) * 2.0 - 0.01);
+            let y = 1.0 + i as f32 * 0.1;
             // We use the rigid_body as a template to insert balls into the world
             rigid_body
-                .set_translation(Vector2::new(x, 100.0))
+                .set_translation(Vector2::new(x, y))
                 .build(&mut physics_world)
                 .handle()
         })
