99bfc154ef1ba631a98cd423c768591df5a85afe
Jared Forsyth <jared@jaredforsyth.com>
2019-08-16 22:53:14 -0600

    refactor out

--- src/main.rs
+++ src/main.rs
@@ -19,12 +19,13 @@ const BALL_RADIUS: f32 = 0.1;
 mod basics;
 mod draw;
 mod groups;
+mod player;
 mod scripting;
 mod skeletons;
 mod sprites;
+mod throw;
 use basics::*;
 use draw::Drawable;
-mod throw;
 use throw::ArrowSys;
 
 struct PhysicsMove;
@@ -36,136 +37,6 @@ impl<'a> System<'a> for PhysicsMove {
     // this runs the system
     fn run(&mut self, (mut physics_world,): Self::SystemData) {
         physics_world.step();
-    }
-}
-
-#[derive(Component)]
-struct Player {
-    down: DefaultColliderHandle,
-    left: DefaultColliderHandle,
-    right: DefaultColliderHandle,
-}
-
-impl Player {
-    fn create_entity(
-        world: &mut World,
-        physics_world: &mut PhysicsWorld<f32>,
-        position: Vector2<f32>,
-    ) {
-        let height = 0.3;
-        let width = 0.1;
-        let offset = 0.05;
-        let mut body = RigidBodyDesc::new().translation(position).build();
-        body.set_rotations_kinematic(true);
-        let rb = physics_world.bodies.insert(body);
-        let collider = ColliderDesc::new(ShapeHandle::new(Capsule::new(height, width)))
-            .density(1.0)
-            .collision_groups(groups::player())
-            .build(BodyPartHandle(rb, 0));
-        // this is the only one that's not in the player group
-        let sensor = ColliderDesc::new(ShapeHandle::new(Capsule::new(height, width)))
-            .sensor(true)
-            .collision_groups(groups::member_all_but_player())
-            .build(BodyPartHandle(rb, 0));
-        let jump_sensor = ColliderDesc::new(ShapeHandle::new(Capsule::new(height, width)))
-            .sensor(true)
-            .collision_groups(groups::player())
-            .translation(Vector2::new(0.0, offset))
-            .build(BodyPartHandle(rb, 0));
-        let left_sensor = physics_world.colliders.insert(
-            ColliderDesc::new(ShapeHandle::new(Capsule::new(height, width)))
-                .sensor(true)
-                .collision_groups(groups::player())
-                .translation(Vector2::new(-offset, 0.0))
-                .build(BodyPartHandle(rb, 0)),
-        );
-        let right_sensor = physics_world.colliders.insert(
-            ColliderDesc::new(ShapeHandle::new(Capsule::new(height, width)))
-                .sensor(true)
-                .collision_groups(groups::player())
-                .translation(Vector2::new(offset, 0.0))
-                .build(BodyPartHandle(rb, 0)),
-        );
-        let cb = physics_world.colliders.insert(collider);
-        let jcb = physics_world.colliders.insert(jump_sensor);
-        let sensor_handle = physics_world.colliders.insert(sensor);
-        world
-            .create_entity()
-            .with(Body(rb))
-            .with(throw::ArrowLauncher(None, sensor_handle))
-            .with(skeletons::component::Skeleton::new("female"))
-            .with(Player {
-                down: jcb,
-                left: left_sensor,
-                right: right_sensor,
-            })
-            .with(Collider(cb))
-            // .with(Drawable::Sprite {
-            //     name: "gnome_head.png".to_owned(),
-            //     scale: 0.4,
-            // })
-            .build();
-    }
-
-    fn can_go_left(&self, physics: &PhysicsWorld<f32>, body: &DefaultBodyHandle) -> bool {
-        for (_handle, collider) in physics
-            .geom
-            .colliders_in_proximity_of(&physics.colliders, self.left)
-            .unwrap()
-        {
-            let bh = collider.body();
-            if &bh == body {
-                continue;
-            }
-            let body = physics.rigid_body(bh).unwrap();
-            if let Some(part) = body.part(0) {
-                if part.is_ground() {
-                    return false;
-                }
-            }
-        }
-        true
-    }
-
-    fn can_go_right(&self, physics: &PhysicsWorld<f32>, body: &DefaultBodyHandle) -> bool {
-        for (_handle, collider) in physics
-            .geom
-            .colliders_in_proximity_of(&physics.colliders, self.right)
-            .unwrap()
-        {
-            let bh = collider.body();
-            if &bh == body {
-                continue;
-            }
-            let body = physics.rigid_body(bh).unwrap();
-            // TODO if there are multiple body parts?
-            if let Some(part) = body.part(0) {
-                if part.is_ground() {
-                    return false;
-                }
-            }
-        }
-        true
-    }
-
-    fn can_jump(&self, physics: &PhysicsWorld<f32>, body: &DefaultBodyHandle) -> bool {
-        for (_handle, collider) in physics
-            .geom
-            .colliders_in_proximity_of(&physics.colliders, self.down)
-            .unwrap()
-        {
-            let bh = collider.body();
-            if &bh == body {
-                continue;
-            }
-            let body = physics.rigid_body(bh).unwrap();
-            if let Some(part) = body.part(0) {
-                if part.is_ground() || part.velocity().linear.y.abs() < 0.1 {
-                    return true;
-                }
-            }
-        }
-        false
     }
 }
 
@@ -208,69 +79,6 @@ impl<'a> System<'a> for GravitySys {
         }
         for entity in to_remove {
             gravities.remove(entity);
-        }
-    }
-}
-
-struct PlayerSys;
-
-impl<'a> System<'a> for PlayerSys {
-    type SystemData = (
-        // Entities<'a>,
-        ReadExpect<'a, raylib::RaylibHandle>,
-        WriteExpect<'a, PhysicsWorld<f32>>,
-        WriteStorage<'a, skeletons::component::Skeleton>,
-        ReadStorage<'a, Body>,
-        ReadStorage<'a, Player>,
-    );
-
-    fn run(&mut self, (rl, mut physics, mut skeletons, body, player): Self::SystemData) {
-        use raylib::consts::KeyboardKey::*;
-
-        let speed = 0.5;
-        let jump_speed = speed * 10.0;
-        let max_speed = 3.0;
-
-        for (body, player, skeleton) in (&body, &player, &mut skeletons).join() {
-            let v = {
-                let body = physics.rigid_body_mut(body.0).unwrap();
-                let part = body.part(0).unwrap();
-                part.velocity().linear
-            };
-
-            let mut push = Vector2::new(0.0, 0.0);
-            if rl.is_key_down(KEY_W) && player.can_jump(&physics, &body.0) && v.y > -jump_speed {
-                let max_jump = -jump_speed - v.y;
-                push.y += max_jump;
-            }
-            if rl.is_key_down(KEY_D) && player.can_go_right(&physics, &body.0) {
-                skeleton.face(skeletons::component::Facing::Right);
-                push.x += speed;
-            }
-            if rl.is_key_down(KEY_A) && player.can_go_left(&physics, &body.0) {
-                skeleton.face(skeletons::component::Facing::Left);
-                push.x -= speed;
-            }
-            if rl.is_key_down(KEY_S) {
-                push.y += speed;
-            }
-            if push.x == 0.0 && push.y == 0.0 {
-                continue;
-            }
-
-            if push.x > 0.0 && v.x > max_speed {
-                push.x = 0.0;
-            }
-            if push.x < 0.0 && v.x < -max_speed {
-                push.x = 0.0;
-            }
-            let body = physics.rigid_body_mut(body.0).unwrap();
-            body.apply_force(
-                0,
-                &nphysics2d::algebra::Force2::linear(push),
-                nphysics2d::algebra::ForceType::VelocityChange,
-                true,
-            );
         }
     }
 }
@@ -342,7 +150,7 @@ fn main() {
     let mut world = World::new();
 
     let mut dispatcher = DispatcherBuilder::new()
-        .with(PlayerSys, "player_move", &[])
+        .with(player::PlayerSys, "player_move", &[])
         .with(
             skeletons::component::SkeletonSys,
             "skeletons",
@@ -405,7 +213,7 @@ fn main() {
             .build();
     }
 
-    Player::create_entity(&mut world, &mut physics_world, Vector2::new(3.0, 1.0));
+    player::Player::create_entity(&mut world, &mut physics_world, Vector2::new(3.0, 1.0));
 
     // Add ground to system
     let ground_handle = physics_world.bodies.insert(Ground::new());
